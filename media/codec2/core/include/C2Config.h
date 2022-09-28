6f81a1e6919e3566f7f9f1212de6848e934296e3
Michael Goffioul <michael.goffioul@gmail.com>
2022-09-28 08:13:21 +0200

    Add media extensions for FFMPEG codec2 service
    A plain buffer is used to transfer extra information from extractor to
    codecs. The buffer is mapped to a custom C2 paramkey.
    
    Change-Id: Id9e36ae90b194b2973ed16af07a9e2b5230aaab7
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    
    Conflicts:
    	media/codec2/core/include/C2Config.h

--- media/codec2/core/include/C2Config.h
+++ media/codec2/core/include/C2Config.h
@@ -303,6 +303,9 @@ enum C2ParamIndexKind : C2Param::type_index_t {
     // input surface
     kParamIndexCaptureFrameRate, // input-surface, float
     kParamIndexStopTimeOffset, // input-surface, int64_t
+
+    // FFMPEG
+    kParamIndexRawCodecData,
 };
 
 }
@@ -2862,6 +2865,11 @@ C2ENUM(C2PlatformConfig::encoding_quality_level_t, uint32_t,
 typedef C2StreamParam<C2Info, C2SimpleValueStruct<int32_t>, kParamIndexAverageBlockQuantization>
         C2AndroidStreamAverageBlockQuantizationInfo;
 constexpr char C2_PARAMKEY_AVERAGE_QP[] = "coded.average-qp";
+
+// FFMEG
+
+typedef C2StreamParam<C2Info, C2BlobValue, kParamIndexRawCodecData> C2StreamRawCodecDataInfo;
+constexpr char C2_PARAMKEY_RAW_CODEC_DATA[] = "coded.raw-codec-data";
 
 /// @}
 
