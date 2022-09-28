34f1d6e9a79b9fe69c7af26c8252d2bf170c029e
Michael Goffioul <michael.goffioul@gmail.com>
2022-09-28 08:13:21 +0200

    Add media extensions for FFMPEG codec2 service
    A plain buffer is used to transfer extra information from extractor to
    codecs. The buffer is mapped to a custom C2 paramkey.
    
    Change-Id: Id9e36ae90b194b2973ed16af07a9e2b5230aaab7

--- media/libstagefright/Utils.cpp
+++ media/libstagefright/Utils.cpp
@@ -832,6 +832,7 @@ static std::vector<std::pair<const char *, uint32_t>> bufferMappings {
         { "slow-motion-markers", kKeySlowMotionMarkers },
         { "thumbnail-csd-av1c", kKeyThumbnailAV1C },
         // FFMEG
+        { "raw-codec-data", kKeyRawCodecData },
         { "raw-codec-specific-data", kKeyRawCodecSpecificData },
     }
 };
