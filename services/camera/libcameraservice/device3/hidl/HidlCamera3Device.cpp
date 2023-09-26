1781aaad8321bb3ea13acaf14fceb104495745b4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-26 20:26:12 +0530

    Camera: Allow setting fallback physical ID for logical camera [1/2]
    On Redmi Note 8 (ginkgo):
    
     * miui camera uses logical id 61 as depth sensor on portrait mode
       but oss libcam maps it to physical id 2 which is wrong, our physical
       id of depth sensor is 20 so we must hack it this way
    
    Set appropriate system property, for example:
    persist.sys.camera.fallback_id_2=20
    
    Change-Id: I58b88adc86dd0036fcbed34a7f20093f90e405dc
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- services/camera/libcameraservice/device3/hidl/HidlCamera3Device.cpp
+++ services/camera/libcameraservice/device3/hidl/HidlCamera3Device.cpp
@@ -29,6 +29,9 @@
 #define CLOGE(fmt, ...) ALOGE("Camera %s: %s: " fmt, mId.c_str(), __FUNCTION__, \
             ##__VA_ARGS__)
 
+#define CLOGW(fmt, ...) ALOGW("Camera %s: %s: " fmt, mId.c_str(), __FUNCTION__, \
+ ##__VA_ARGS__)
+
 // Convenience macros for transitioning to the error state
 #define SET_ERR(fmt, ...) setErrorState(   \
     "%s: " fmt, __FUNCTION__,              \
@@ -48,6 +51,7 @@
 #include <cutils/properties.h>
 #include <camera/StringUtils.h>
 #include <com_android_internal_camera_flags.h>
+#include <android-base/properties.h>
 
 #include <android/hardware/camera/device/3.7/ICameraInjectionSession.h>
 #include <android/hardware/camera2/ICameraDeviceUser.h>
@@ -184,10 +188,22 @@ status_t HidlCamera3Device::initialize(sp<CameraProviderManager> manager,
                     physicalId, /*overrideForPerfClass*/false, &mPhysicalDeviceInfoMap[physicalId],
                     hardware::ICameraService::ROTATION_OVERRIDE_NONE);
             if (res != OK) {
-                SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
-                        physicalId.c_str(), strerror(-res), res);
-                session->close();
-                return res;
+                auto fallbackId = base::GetProperty(
+                        "persist.sys.camera.fallback_id_" + physicalId, "");
+                if (!fallbackId.empty()) {
+                    CLOGW("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    CLOGW("Trying fallback camera %s", fallbackId.c_str());
+                    res = manager->getCameraCharacteristics(
+                            fallbackId, /*overrideForPerfClass*/false,
+                            &mPhysicalDeviceInfoMap[fallbackId], /*overrideToPortrait*/true);
+                }
+                if (res != OK) {
+                    SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    session->close();
+                    return res;
+                }
             }
 
             bool usePrecorrectArray =
