604f5c81052c4328a906d2964e9d58a4472880ea
Ido Ben-Hur <idoybh2@gmail.com>
2025-02-05 11:32:45 +0200

    audioflinger: Adapt audio fix for wifi display to A15/r14
    see 361033f2d46bd0673b650100823b81298c1a4e82
    
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
@@ -909,11 +909,12 @@ Status AudioPolicyService::startInput(int32_t portIdAidl)
     msg << "Audio recording on session " << client->session;
 
     // check calling permissions
-    if (!(startRecording(client->attributionSource, client->virtualDeviceId,
-                         String16(msg.str().c_str()), client->attributes.source)
+    if (!isAudioServerOrMediaServerUid(client->attributionSource.uid) &&
+            (!(startRecording(client->attributionSource, client->virtualDeviceId,
+                              String16(msg.str().c_str()), client->attributes.source)
             || client->attributes.source == AUDIO_SOURCE_FM_TUNER
             || client->attributes.source == AUDIO_SOURCE_REMOTE_SUBMIX
-            || client->attributes.source == AUDIO_SOURCE_ECHO_REFERENCE)) {
+            || client->attributes.source == AUDIO_SOURCE_ECHO_REFERENCE))) {
         ALOGE("%s permission denied: recording not allowed for attribution source %s",
                 __func__, client->attributionSource.toString().c_str());
         return binderStatusFromStatusT(PERMISSION_DENIED);
